827265aea6d9356646e596d24e302a8383781120
Santi Castells <santi.castells@fancybox.com>
2013-09-25 11:52:15 -0300

    add class

---
+++ PagosonlineBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Scastells\PagosonlineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+/**
+ * Pagosonline payment bundle
+ */
+class PagosonlineBundle extends Bundle
+{
+
+}
