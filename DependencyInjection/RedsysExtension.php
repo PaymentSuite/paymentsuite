307e5de5766b12127115701fd4f6190a10b1a007
Gonzalo Vilaseca <gonzalo.vilaseca@gmail.com>
2014-01-27 14:18:33 +0100

    ajustes

--- DependencyInjection/RedsysExtension.php
+++ DependencyInjection/RedsysExtension.php
@@ -23,6 +23,7 @@ class RedsysExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
         $loader->load('services.yml');
     }
 }
