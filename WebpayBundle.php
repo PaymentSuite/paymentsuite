d81e2691da7017efb0528f052047616bb9b18dd0
David Pujadas <david.pujadas@fancybox.com>
2014-02-26 15:18:48 -0300

    Add composer.json

---
+++ WebpayBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * WebpayBundle for Symfony2
+ *
+ * This Bundle is part of Symfony2 Payment Suite
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @package WebpayBundle
+ *
+ * Marc Morera 2013
+ */
+
+namespace PaymentSuite\WebpayBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Webpay payment bundle
+ */
+class WebpayBundle extends Bundle
+{
+
+}
