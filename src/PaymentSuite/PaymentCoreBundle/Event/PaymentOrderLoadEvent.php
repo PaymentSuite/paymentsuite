b0f2cadf7e66be43a340cb7abfa7f83e80e1d5c3
mmoreram <marc.morera@befactory.es>
2016-01-19 18:34:43 +0100

    Updated copyright in all files

--- src/PaymentSuite/PaymentCoreBundle/Event/PaymentOrderLoadEvent.php
+++ src/PaymentSuite/PaymentCoreBundle/Event/PaymentOrderLoadEvent.php
@@ -3,6 +3,8 @@
 /*
  * This file is part of the PaymentSuite package.
  *
+ * Copyright (c) 2013-2016 Marc Morera
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
